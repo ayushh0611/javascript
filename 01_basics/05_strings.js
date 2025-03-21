cea9e50dc2e9785463b77553adb2627fbdddeac0
Ayush Singh <singhayush0611@gmail.com>
2025-03-21 18:55:52 +0530

    Add files via upload

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+// const name="ayush"
+// const repoCount = 50
+// console.log(name + repoCount + "Value")
+
+// nowadays backticks ``; string interpolation is used i.e. we make placeholders and can inject values directly into them
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("hitesh-hc-com")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('u'));
+
+// const newString = gameName.substring(0,4)  //cannot use negative value in substring method
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+// const newStringOne= "   ayush   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());       //removes spaces
+
+// const url= "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('ayush'));       //checks the presence of value passed
+// console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));      // splits into array where - is present
